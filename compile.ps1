e5c8cf33bfc2b076498db9cf2a782820ead4336e
weefbellington <weefbellington@gmail.com>
2024-01-03 21:08:44 -0800

    Prepare for 1.0.0 release.

---
+++ compile.ps1
@@ -0,0 +1,17 @@
+
+$FALLOUT4_DATA = "$env:FALLOUT4_PATH\Data"
+$FALLOUT4_SCRIPTS = "$FALLOUT4_DATA\Scripts"
+$FALLOUT4_SCRIPTS_SOURCE_USER = "$FALLOUT4_SCRIPTS\Source\User"
+$FALLOUT4_SCRIPTS_SOURCE_BASE = "$FALLOUT4_SCRIPTS\Source\Base"
+
+$IMPORT_PATH = "$FALLOUT4_SCRIPTS;$FALLOUT4_SCRIPTS_SOURCE_USER;$FALLOUT4_SCRIPTS_SOURCE_BASE"
+$BUILD_PATH = "$PSScriptRoot\build\"
+$BUILD_PATH_DEBUG = "$BUILD_PATH\debug\Data\Scripts"
+$BUILD_PATH_RELEASE = "$BUILD_PATH\release\Data\Scripts"
+
+$INSTITUTE_FLAGS_FILE = "$FALLOUT4_SCRIPTS_SOURCE_BASE\Institute_Papyrus_Flags.flg"
+
+PapyrusCompiler robco-auto-sort.ppj -import="$IMPORT_PATH" -output="$BUILD_PATH_DEBUG" -flags="$INSTITUTE_FLAGS_FILE"
+PapyrusCompiler robco-auto-sort.ppj -import="$IMPORT_PATH" -output="$BUILD_PATH_RELEASE" -flags="$INSTITUTE_FLAGS_FILE" -optimize -release -final
+
+Copy-Item -Path "$BUILD_PATH\debug\Data" -DESTINATION "$env:FALLOUT4_PATH" -RECURSE -FORCE
